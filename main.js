63c5eb90432fb72a1c7ffc100a863cf78f4a842c
ghislaingenay <ghislain.gny@gmail.com>
2023-09-28 13:29:42 +0700

    render scene on the page

--- main.js
+++ main.js
@@ -11,3 +11,21 @@ const geometry = new THREE.SphereGeometry(1, 64, 64);
 const material = new THREE.MeshStandardMaterial({ color: "#00ff83" });
 const mesh = new THREE.Mesh(geometry, material); // combine material and geometry
 scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  45, // Field of view
+  window.innerWidth / window.innerHeight // Aspect ratio
+  // 0.1, // Near clipping plane
+  // 1000 // Far clipping plane
+);
+
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
