de7716b79ead9f644fc8454620473a61f262cbe9
ghislaingenay <ghislain.gny@gmail.com>
2023-09-28 13:51:10 +0700

    create basic ball

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 import * as THREE from "three";
 
+const width = 800;
+const height = 600;
+const aspectRatio = width / height;
+
 // Scene
 const scene = new THREE.Scene();
 
 // Creating a sphere
-const geometry = new THREE.SphereGeometry(1, 64, 64);
+const geometry = new THREE.SphereGeometry(3, 64, 64);
 // Radius: size of the sphere
 // Width segments: number of horizontal segments
 // Height segments: number of vertical segments
@@ -12,14 +16,19 @@ const material = new THREE.MeshStandardMaterial({ color: "#00ff83" });
 const mesh = new THREE.Mesh(geometry, material); // combine material and geometry
 scene.add(mesh);
 
+// Lights
+const pointLight = new THREE.PointLight(0xffffff, 100, 100, 1.5); // decay: The amount the light dims along the distance of the light.
+pointLight.position.set(0, 10, 10);
+scene.add(pointLight);
+
 // Camera
 const camera = new THREE.PerspectiveCamera(
-  45, // Field of view
-  window.innerWidth / window.innerHeight // Aspect ratio
-  // 0.1, // Near clipping plane
-  // 1000 // Far clipping plane
+  50, // Field of view
+  aspectRatio, // Aspect ratio
+  0.1, // Near clipping plane
+  150 // Far clipping plane => like MineCraft
 );
-
+camera.position.z = 20;
 scene.add(camera);
 
 // Renderer
@@ -27,5 +36,5 @@ const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({
   canvas,
 });
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(width, height);
 renderer.render(scene, camera);
